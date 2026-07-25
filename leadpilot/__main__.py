317f11db447f015ee22769dfd8305dc0f729e3eb
digitalwaveai <v5163219@gmail.com>
2026-07-25 22:14:48 +0300

    Enable niche questionnaires and project search context

--- leadpilot/__main__.py
+++ leadpilot/__main__.py
@@ -3,6 +3,8 @@ from .bot import LeadPilotBot, run
 from .database import Database
 from .global_price_mode import install_global_price_mode
 from .personal_lead_ids import install_personal_lead_ids
+from .project_questionnaires import install_project_questionnaires
+from .project_search_context import install_project_search_context
 from .role_policy import install_role_policy
 from .search_quality import install_search_quality
 from .serpapi import SerpApiClient
@@ -12,8 +14,10 @@ from .user_commands import install_user_commands
 
 install_beta_access(LeadPilotBot, Database)
 install_personal_lead_ids(Database)
+install_project_questionnaires(LeadPilotBot, Database)
 install_usage_limits(LeadPilotBot, Database)
 install_search_quality(LeadPilotBot, SerpApiClient)
+install_project_search_context(LeadPilotBot, SerpApiClient)
 install_role_policy(LeadPilotBot)
 install_global_price_mode(LeadPilotBot)
 install_user_commands(LeadPilotBot)
