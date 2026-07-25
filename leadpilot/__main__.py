085d2f0f2cf477872b51da2aabc32f349f06b15d
digitalwaveai <v5163219@gmail.com>
2026-07-25 21:36:50 +0300

    Enable strict lead search quality checks

--- leadpilot/__main__.py
+++ leadpilot/__main__.py
@@ -4,6 +4,8 @@ from .database import Database
 from .global_price_mode import install_global_price_mode
 from .personal_lead_ids import install_personal_lead_ids
 from .role_policy import install_role_policy
+from .search_quality import install_search_quality
+from .serpapi import SerpApiClient
 from .usage_limits import install_usage_limits
 from .user_commands import install_user_commands
 
@@ -11,6 +13,7 @@ from .user_commands import install_user_commands
 install_beta_access(LeadPilotBot, Database)
 install_personal_lead_ids(Database)
 install_usage_limits(LeadPilotBot, Database)
+install_search_quality(LeadPilotBot, SerpApiClient)
 install_role_policy(LeadPilotBot)
 install_global_price_mode(LeadPilotBot)
 install_user_commands(LeadPilotBot)
