16a2d9beb3cb664b9dde2f670c849a5cfe6fc95c
digitalwaveai <v5163219@gmail.com>
2026-07-25 21:17:42 +0300

    Enable usage tracking and personal lead IDs

--- leadpilot/__main__.py
+++ leadpilot/__main__.py
@@ -2,11 +2,15 @@ from .beta_access import install_beta_access
 from .bot import LeadPilotBot, run
 from .database import Database
 from .global_price_mode import install_global_price_mode
+from .personal_lead_ids import install_personal_lead_ids
 from .role_policy import install_role_policy
+from .usage_limits import install_usage_limits
 from .user_commands import install_user_commands
 
 
 install_beta_access(LeadPilotBot, Database)
+install_personal_lead_ids(Database)
+install_usage_limits(LeadPilotBot, Database)
 install_role_policy(LeadPilotBot)
 install_global_price_mode(LeadPilotBot)
 install_user_commands(LeadPilotBot)
