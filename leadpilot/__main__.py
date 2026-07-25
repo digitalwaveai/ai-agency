e2f8d1e39cd322109a82c6ad25db0369e6fc60bc
digitalwaveai <v5163219@gmail.com>
2026-07-25 16:40:23 +0300

    Enable owner-only role management policy

--- leadpilot/__main__.py
+++ leadpilot/__main__.py
@@ -1,7 +1,9 @@
 from .bot import LeadPilotBot, run
+from .role_policy import install_role_policy
 from .user_commands import install_user_commands
 
 
+install_role_policy(LeadPilotBot)
 install_user_commands(LeadPilotBot)
 
 
