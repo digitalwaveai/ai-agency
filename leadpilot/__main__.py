8e88b2004cb561352aa448cd0a0e222b023fbf2e
digitalwaveai <v5163219@gmail.com>
2026-07-25 16:49:06 +0300

    Enable automatic beta access expiration

--- leadpilot/__main__.py
+++ leadpilot/__main__.py
@@ -1,8 +1,11 @@
+from .beta_access import install_beta_access
 from .bot import LeadPilotBot, run
+from .database import Database
 from .role_policy import install_role_policy
 from .user_commands import install_user_commands
 
 
+install_beta_access(LeadPilotBot, Database)
 install_role_policy(LeadPilotBot)
 install_user_commands(LeadPilotBot)
 
